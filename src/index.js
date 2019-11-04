fd9d789ac2407869582bd147ef628f989b7425ca
Cristian Galeano <cristian.galeano1913@gmail.com>
2019-11-04 12:34:47 -0500

    Cors

--- src/index.js
+++ src/index.js
@@ -25,6 +25,14 @@ app.use("/api/nasa", require("./routes/nasa"));
 
 app.use(express.static(`${__dirname}/public`));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, X-SOCKET-ID, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 //Server Listen
 app.listen(app.get("port"), () => {
   //console.log(`Server Listening in port ${app.get("port")}`);
