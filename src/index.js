3342c019a7c5addd5d3dce7da9f9ebe9b990f02b
Cristian Galeano <cristian.galeano1913@gmail.com>
2019-11-21 07:09:16 -0500

    Force Https

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const app = express();
 const morgan = require("morgan");
 const config = require('./config')
 const db = require('./database')
+const secure = require('express-force-https');
+const node_env = process.env.NODE_ENV || 'development';
+
 config.BASE_PATH = __dirname;
 
 //settings
@@ -16,6 +19,11 @@ app.set("port", process.env.PORT || config.PORT);
 app.use(morgan("dev"));
 app.use(express.json());
 
+if (node_env == 'production') {
+	console.log(node_env)
+	app.use(secure)
+};
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, X-SOCKET-ID, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
@@ -23,6 +31,8 @@ app.use((req, res, next) => {
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
+
+
 
 //routes
 
